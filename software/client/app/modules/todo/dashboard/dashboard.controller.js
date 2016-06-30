90b741b669038e7c6c8538cf89917cfd0a6b2c6f
sukeshratnalu <sukesh.ratnalu@india.semanticbits.com>
2016-06-30 15:54:37 +0530

    commit

--- software/client/app/modules/todo/dashboard/dashboard.controller.js
+++ software/client/app/modules/todo/dashboard/dashboard.controller.js
@@ -7,6 +7,7 @@
         .module('todoApp')
         .controller('dashboardController',dashboardController);
 
+
     dashboardController.$inject=['$scope','$q','$rootScope','dashboardService','$translate','$filter','$sce','$uibModal','$log'];
 
     function dashboardController($scope,$q,$rootScope,dashboardService,$translate,$filter,$sce,$uibModal,$log){
@@ -14,10 +15,20 @@
         dc.tasks=[];
         dc.selectedStatus={};
         dc.selectedPriorites=[];
-        //dc.selectedStatus.value=[];
         dc.filterTask=filterTask;
-        dc.deleteTasks=deleteTasks;
-        dc.markAsCompleteTask=markAsCompleteTask;
+        dc.priorityList = [
+            { name:'High', value : 'HIGH'},
+            { name:'Medium',value : "MEDIUM"},
+            { name:'Low',value : "LOW"}
+
+        ];
+        dc.statusList = [
+            { name:'Opened' ,value: 'OPENED'},
+            { name:'Inprogress', value:'INPROGRESS'},
+            { name:'Invalid' ,value:'INVALID'},
+            { name:'Completed', value:'COMPLETED'}
+
+        ];
         //getting tasks from services
         var promise = dashboardService.readTasks();
         promise.then(function(response) {
@@ -29,19 +40,7 @@
         }, function(reason) {
             alert('Failed: ' + reason);
         });
-        $scope.priorityList = [
-            { name:'High', value : 'HIGH'},
-            { name:'Medium',value : "MEDIUM"},
-            { name:'Low',value : "LOW"}
 
-        ];
-        $scope.statusList = [
-            { name:'Opened' ,value: 'OPENED'},
-            { name:'Inprogress', value:'INPROGRESS'},
-            { name:'Invalid' ,value:'INVALID'},
-            { name:'Completed', value:'COMPLETED'}
-
-        ];
         //function for filtering the task based on status and priority(single selecte for status and multiselect for priority)
         function filterTask(){
             dc.tasks =[];
@@ -79,104 +78,11 @@
                 dc.tasks=$rootScope.totalTasks;
             }
         }
-        //function for delete task
-        function deleteTasks(name) {
 
-            dc.openDeleteModal(name);
 
-        }
-        //function for mark as complete
-        function markAsCompleteTask(name){
-
-            dc.openCheckModal(name);
-        }
-        //logic for delete task and creating delete modal
-        dc.items = ['Are you want to delete the task?'];
-        dc.animationsEnabled = true;
-        dc.openDeleteModal = function (name) {
-            var modalInstance = $uibModal.open({
-                animation: dc.animationsEnabled,
-                templateUrl: 'partials/deleteModal.html',
-                controller: function($uibModalInstance ,$scope,items){
-
-                    dc.items = items;
-                    dc.selected = {
-                        item: dc.items[0]
-                    };
-                    $scope.ok = function () {
-
-                        var deleteTask = dc.tasks;
-                        dc.tasks=deleteTask;
-                        for (var i = 0; i < deleteTask.length; i++) {
-                            if (deleteTask[i].name == name) {
-                                deleteTask.splice(i, 1);
-                                break;
-                            }
-                            console.log(deleteTask[i].name);
-                        }
-                        dc.tasks = deleteTask;
-                        $uibModalInstance.close(dc.selected.item);
-                    };
-
-                    $scope.cancel = function () {
-                        $uibModalInstance.dismiss('cancel');
-                    };
-
-                },
-                resolve: {
-                    items: function () {
-                        return dc.items;
-                    }
-                }
-            });
-            modalInstance.result.then(function (selectedItem) {
-                dc.selected = selectedItem;
-            }, function () {
-                $log.info('Modal dismissed at: ' + new Date());
-            });
-        };
-        //logis for mark as complete and creating mark as complete modal
-        dc.items = ['Do you want to make as complete?'];
-        dc.openCheckModal = function (name) {
-            var modalInstance = $uibModal.open({
-                animation: dc.animationsEnabled,
-                templateUrl: 'partials/checkModal.html',
-                controller: function($uibModalInstance ,$scope,items){
-                    dc.items = items;
-                    dc.selected = {
-                        item: dc.items[0]
-                    };
-                    $scope.ok = function () {
-                        var makeAsComplete=dc.tasks;
-                            for (var i = 0; i < makeAsComplete.length; i++) {
-                                if (makeAsComplete[i].name === name) {
-                                    makeAsComplete[i].status="COMPLETED";
-                                    break;
-                                }
-                            }
-                            dc.tasks = makeAsComplete;
-                        $uibModalInstance.close(dc.selected.item);
-                    };
-                    $scope.cancel = function () {
-                        $uibModalInstance.dismiss('cancel');
-                    };
-                },
-                resolve: {
-                    items: function () {
-                        return dc.items;
-                    }
-                }
-            });
-            modalInstance.result.then(function (selectedItem) {
-                dc.selected = selectedItem;
-            }, function () {
-                $log.info('Modal dismissed at: ' + new Date());
-            });
-        };
-        dc.toggleAnimation = function () {
-            dc.animationsEnabled = !dc.animationsEnabled;
-        };
     }
+
+
 
 }());
 
